2938844b81d3d5d5ab42354155c7adc721a3b5fa
Thomas Sarboni <max-k@post.com>
2014-08-01 10:55:55 +0200

    DEPLOY-4: Handle proxy mode in obm-autoconf role

--- build-resources-dir.sh
+++ build-resources-dir.sh
@@ -24,12 +24,27 @@ if [ ! -f jdk-6u45-linux-x64-rpm.bin ]; then
     wget -q http://uni-smr.ac.ru/archive/dev/java/SDKs/sun/j2se/6/jdk-6u45-linux-x64-rpm.bin
 fi
 echo "### Download obm-release RPM ###"
-if [ ! -f obm-release.noarch.rpm ]; then
+if [ ! -f obm-release-2.5-1.noarch.rpm ]; then
     wget -q http://packages.obm.org/rpm/25/release/obm-release-2.5-1.noarch.rpm
 fi
 echo "### Download PG91 repo RPM ###"
 if [ ! -f pgdg-centos91-9.1-4.noarch.rpm ]; then
     wget -q http://yum.postgresql.org/9.1/redhat/rhel-6-x86_64/pgdg-centos91-9.1-4.noarch.rpm
 fi
+echo "### Create lightning xpi directory ###"
+mkdir -p xpi/lightning/tb{24,31}
+echo "### Download lightning xpis ###"
+lightning_base_url="http://download.obm.org/mozilla-addons/lightning/stable"
+wget -qN ${lightning_base_url}/tb24/latest-linux.xpi -O xpi/lightning/tb24/latest-linux.xpi
+wget -qN ${lightning_base_url}/tb24/latest-mac.xpi -O xpi/lightning/tb24/latest-mac.xpi
+wget -qN ${lightning_base_url}/tb24/latest-windows.xpi -O xpi/lightning/tb24/latest-windows.xpi
+wget -qN ${lightning_base_url}/tb31/latest-linux.xpi -O xpi/lightning/tb31/latest-linux.xpi
+wget -qN ${lightning_base_url}/tb31/latest-mac.xpi -O xpi/lightning/tb31/latest-mac.xpi
+wget -qN ${lightning_base_url}/tb31/latest-windows.xpi -O xpi/lightning/tb31/latest-windows.xpi
+echo "### Create obm connector xpi directory ###"
+mkdir -p xpi/connector/tb{24,31}
+echo "### Download obm connector xpis ###"
+connector_base_url="http://download.obm.org/mozilla-addons/obm-connector/stable"
+wget -qN ${connector_base_url}/tb24/obm-connector-latest.xpi -O xpi/connector/tb24/obm-connector-latest.xpi
 popd
 echo "### DONE ! ###"
